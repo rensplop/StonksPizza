177159b7c8b46a50658a4773d51ea96afb339a4d
Willem de Wit <ps253300@edu.summacollege.nl>
2024-12-19 11:13:10 +0100

    Updated the index

--- StonksPizza/resources/views/Pizzaria/Index.blade.php
+++ StonksPizza/resources/views/Pizzaria/Index.blade.php
@@ -1,17 +1,15 @@
-Route::get('/', function () {
-    return response()->make('
-        <!DOCTYPE html>
+<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Test</title>
+            <title>Pizzeria</title>
             <style>
                 body {
                     margin: 0;
                     display: flex;
                     justify-content: center;
-                    align-items: center;
+                    align-items: top;
                     height: 100vh;
                     background-color: #000;
                     color: #fff;
@@ -35,7 +33,6 @@ Route::get('/', function () {
             </style>
         </head>
         <body>
-            <h1>Test</h1>
+            <h1>Pizzeria</h1>
         </body>
-        </html>', 200, ['Content-Type' => 'text/html']);
-});
+        </html>
