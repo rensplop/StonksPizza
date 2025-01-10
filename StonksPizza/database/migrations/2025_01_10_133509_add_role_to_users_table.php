dbb2e7ddfba65ed4346648340c251436cee8de2c
Willem de Wit <ps253300@edu.summacollege.nl>
2025-01-10 14:36:53 +0100

    is admin functie

--- StonksPizza/database/migrations/2025_01_10_133509_add_role_to_users_table.php
+++ StonksPizza/database/migrations/2025_01_10_133509_add_role_to_users_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('user')->after('email'); // Default: 'user'
+            $table->string('role')->default('user')->after('email'); 
         });
     }
 
