1a0d6b2c91ef0814c4944b83dfa4dce794f70e1e
Willem Beaujean <ps255516@summacollege.nl>
2025-01-17 11:41:15 +0100

    userseeder fix

--- StonksPizza/database/seeders/DatabaseSeeder.php
+++ StonksPizza/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             IngredientSeeder::class,
             SizeSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
