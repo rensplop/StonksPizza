0004313fb1277f0e9ab9863d9c6df81828305d9b
Willem Beaujean <ps255516@summacollege.nl>
2025-01-20 11:42:09 +0100

    bestelling pagina laat nu lijst aan pizzas zien

--- StonksPizza/resources/views/Orders/Index.blade.php
+++ StonksPizza/resources/views/Orders/Index.blade.php
@@ -31,7 +31,25 @@
     </header>
 
 <div class="container max-w-screen-xl mx-auto px-4">
-    <p>hxdsf</p>
+    @auth
+    <div class="p-6 rounded-lg">
+    <h1 class="text-xl font-bold mb-4 text-center">My Order</h1>
+    @foreach ($pizzas as $pizza)
+        <div class="bg-white rounded-lg shadow-md p-4 flex justify-between items-center mb-4">
+            <div>
+                <h2 class="text-lg font-bold">{{ $pizza->naam }}</h2>
+            </div>
+            <div class="text-lg font-semibold text-gray-800">
+                &euro;{{ number_format($pizza->prijs, 2, ',', '.') }}
+            </div>
+        </div>
+    @endforeach
+</div>
+    @else
+    <p class="text-center text-lg mt-10">
+        <strong>Log in</strong> om het menu te bekijken.
+    </p>
+    @endauth
 </div>
 
 <footer class="bg-gray-800 text-gray-400 text-center py-4">
--- StonksPizza/routes/web.php
+++ StonksPizza/routes/web.php
@@ -17,7 +17,7 @@ Route::resource('voertuigen', VoertuigController::class)->parameters([
     'voertuigen' => 'voertuig'
 ]);
 
-Route::get('/bestelling', [BestellingController::class, 'index'])->name('orders.index');
+Route::get('/bestelling', [BestelregelController::class, 'index'])->name('orders.index');
 
 Route::get('/menu', [PizzaController::class, 'index'])->name('menu.index');
 Route::resource('pizza', PizzaController::class)->except(['index']);
@@ -68,9 +68,7 @@ Route::get('/login', function () {
     return view('auth.login'); 
 })->name('login');
 
-Route::get('/bestelling', function () {
-    return view('orders.index'); 
-})->name('bestelling');
+
 
 Route::get('/logout', function () {
     Auth::logout();
